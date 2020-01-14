693f72f9745c3afe736551dcb080dce85fef71ea
Rade58 <bajic.rade2@gmail.com>
2020-01-14 14:27:22 +0100

    data is normalized

--- src/normalized_data.js
+++ src/normalized_data.js
@@ -1,16 +1,16 @@
-/* import normalizr from 'normalizr'
+import normalizr from 'normalizr'
 import * as fetchedData from './gathered_data.json'
- */
+
 // for node testing
-const normalizr = require('normalizr')
-const fetchedData = require('./gathered_data.json')
+/* const normalizr = require('normalizr')
+const fetchedData = require('./gathered_data.json') */
 //
 
 // USERS AND CARDS ISOLATED 
 
 const arrayOfUsers = fetchedData.users
 const arrayOfLists = fetchedData.lists
-// EVERY LIST OBHECT, HAS ALOSO A cards ARRAY
+// EVERY LIST OBJECT, HAS ALOSO A cards ARRAY
 
 
 // NORMALAZING DATA
@@ -20,16 +20,51 @@ const userSchema = new normalizr.schema.Entity('users')         // STRING ARGUME
 const cardSchema = new normalizr.schema.Entity('cards', {assignedTo: userSchema})
 
 const listSchema = new normalizr.schema.Entity('lists', {
-  cards: [cardSchema]
+  cards: [cardSchema]         // THIS YOU'LL HAVE TWO entities INSIDE OBJECT
 })
 
 // EVERY OBJECT WILL HAVE entities PROPERTY WITH KEY VALUE PAIRS
 
 // IF YOU WRAP SHEMA IN ARRAY (SECOND ARGUMENT) YOU HAVE ARRAY result ,AND INSIDE WILL BE IDs
 
-const users = normalizr.normalize(arrayOfUsers, [userSchema])
+const normalizedUsers = normalizr.normalize(arrayOfUsers, [userSchema]) //  { result: [**USER IDS INSIDE**],   entities: { users: {  **KEY(ID)/VALUE PAIRS**  }   }   }
 
-const lists = normalizr.normalize(arrayOfLists, [listSchema])
+const normalizedLists = normalizr.normalize(arrayOfLists, [listSchema]) //  {   result: [**LIST IDS INSIDE**],   
+//                                                                    entities: { 
+//                                                                      lists: {  **KEY(ID)/VALUE PAIRS**  }, cards: {  **KEY(ID)/VALUE PAIRS** }
+//                                                                    }   
+//                                                                }
 
-console.log({users, lists})
+// TO ACCESS normalizedList.entities.lists    NOT IDEAL (SAME GOES FOR USERS TOO)
 
+// ALSO IDS ARE UNDER result (NOT SO SUGESTIVE)
+
+//   ALSO CARDS DON'T HVE ARRAY OF THEIR IDs
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+const users = {
+  entities: normalizedUsers.entities.users,
+  ids: normalizedUsers.result
+}
+
+const lists = {
+  entities: normalizedLists.entities.lists,
+  ids: normalizedLists.result
+}
+
+const cards = {
+  entities: normalizedLists.entities.cards,
+  ids: Object.keys(normalizedLists.entities.cards)
+}
+
+
+
+// console.log({users: normalizedUsers, lists: normalizedLists})
+// console.log({cards: normalizedLists.entities.cards})
+
+export default {
+  users,
+  lists,
+  cards
+}
